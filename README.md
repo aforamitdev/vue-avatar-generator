746ec219c52bbe196727eede71dd6fb6b610e98a
activationeuron <aformaitrai@gmail.com>
2020-02-07 12:52:34 +0530

    avatar conpleted

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# itenticon
+## vue js avatar generator using jdenticon
 
-## Project setup
-```
-pnpm install
-```
-
-### Compiles and hot-reloads for development
-```
-pnpm run serve
-```
-
-### Compiles and minifies for production
-```
-pnpm run build
-```
-
-### Lints and fixes files
-```
-pnpm run lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+![avatar](./avator.png)
--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+<div class="bg-gray-300 min-h-screen text-center py-10">
+  <div class="text-xl text-semibold p-6 text-gray-800">Enter Your favourite string</div>
+  <input type="text" class="border rounded-lg px-3 py-3 bg-gray-600 text-gray-100 focus:bg-gray-200 focus:text-gray-800" placeholder="Enter string" v-model="input" v-on="onInput()">
+  
+  <div class=" flex items-center justify-center py-10" v-html="identicon"></div>
+  <div class="container mx-auto border border-blue-900 bg-gray-400 p-4">
+    {{identicon}}
   </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
+import jdenticon from "jdenticon";
 export default {
   name: 'app',
   components: {
-    HelloWorld
+  },
+  data:()=>{
+    return {
+      input:""
+    }
+    
+  },
+  methods:{
+    onInput:function(){
+      console.log(this.input)
+    }
+  },
+  computed:{
+    identicon:function(){
+      return jdenticon.toSvg(this.input,200)
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
